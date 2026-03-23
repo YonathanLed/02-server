48820388714b30c7451979d65c6688dc181faf0e
Yonathan Lederman <ionathanl@wix.com>
2026-03-23 18:26:11 +0200

    My first server

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/about', (req, res) => {
+  res.send("I'm Yonathan Lederman")
+})
+
+app.get('/api/time', (req, res) => {
+  res.json({
+    time: new Date().toLocaleTimeString(),
+    date: new Date().toLocaleDateString()
+  });
+});
+
+app.get('/api/greeting', (req, res) => {
+  const name = req.query.name || 'World';
+  res.json({
+    greeting: `Hello, ${name}!`
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
